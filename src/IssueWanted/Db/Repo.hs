8d7eb5f30ccc3a894e1e6acc4aa8d02e0fbf1870
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-09-23 13:55:25 +0500

    Update Schema, add setup and add output

--- src/IssueWanted/Db/Repo.hs
+++ src/IssueWanted/Db/Repo.hs
@@ -4,34 +4,99 @@
 
 module IssueWanted.Db.Repo where
 
-import Squeal.PostgreSQL ((:::), NullityType (..), PGType (..), (:=>))
-import Squeal.PostgreSQL.Schema ( ColumnConstraint(..), SchemumType(..), TableConstraint(..))
+import Squeal.PostgreSQL ((:::), (:=>), ColumnConstraint (..), Definition, NP (..),
+                          NullityType (..), OnDeleteClause (..), OnUpdateClause (..), PGType (..),
+                          SchemumType (..), TableConstraint (..), as, createTable, foreignKey, int,
+                          notNullable, nullable, primaryKey, serial, text)
+
+import qualified Squeal.PostgreSQL as SP
+
 
 type Schema =
-    '[ "repos"  ::: 'Table ( -- all Haskell repos
+    -- all Haskell repos
+    '[ "repos"  ::: 'Table (
         '[ "pk_repo" ::: 'PrimaryKey '["repoId"] ] :=>
-        '[ "repoId"       ::: 'NoDef :=> 'NotNull 'PGint4
+        '[ "repoId"       ::: 'Def :=> 'NotNull 'PGint4
+         , "repoName"     ::: 'NoDef :=> 'NotNull 'PGtext
          , "repoUrl"      ::: 'NoDef :=> 'NotNull 'PGtext
-         , "repoName"     ::: 'Def   :=> 'NotNull 'PGtext
-         , "repoOwner"    ::: 'Def   :=> 'NotNull 'PGtext
-         , "repoIssueHW"  ::: 'Def   :=> 'NotNull 'PGbool -- Repo has issues labeled "HelpWanted"
-         , "repoIssueGFI" ::: 'Def   :=> 'NotNull 'PGbool -- Repo has issues labeled "Good First Issue"
+         , "repoOwner"    ::: 'NoDef :=> 'NotNull 'PGtext
+         , "repoIssueHW"  ::: 'NoDef :=> 'NotNull 'PGbool -- Repo has issues labeled "HelpWanted"
+         , "repoIssueGFI" ::: 'NoDef :=> 'NotNull 'PGbool -- Repo has issues labeled "Good First Issue"
         ])
-    , "repoIssueGFI" ::: 'Table ( -- all Haskell issues labeled "Good First Issue"
-        '[ "pk_gfi"     ::: 'PrimaryKey '["gfiUrl"]
+    -- all Haskell issues labeled "Good First Issue"
+    , "repoIssueGFI" ::: 'Table (
+        '[ "pk_gfi"     ::: 'PrimaryKey '["gfiId"]
          , "fk_repo_id" ::: 'ForeignKey '["gfiRepoId"] "repos" '["repoId"]
          ] :=>
-        '[ "gfiTitle"  ::: 'Def   :=> 'NotNull ' PGtext
-         , "gfiUrl"    ::: 'NoDef :=> 'NotNull 'PGtext
+        '[ "gfiId"     ::: 'Def   :=> 'NotNull 'PGint4
          , "gfiRepoId" ::: 'NoDef :=> 'NotNull 'PGint4
+         , "gfiTitle"  ::: 'NoDef   :=> 'Null 'PGtext
+         , "gfiUrl"    ::: 'NoDef   :=> 'Null 'PGtext
         ])
-
-    , "repoIssueHW" ::: 'Table ( -- all Haskell issues labeled "Help Wanted"
-        '[ "pk_hw"      ::: 'PrimaryKey '["hwUrl"]
+    -- all Haskell issues labeled "Help Wanted"
+    , "repoIssueHW" ::: 'Table (
+        '[ "pk_hw"      ::: 'PrimaryKey '["hwId"]
          , "fk_repo_id" ::: 'ForeignKey '["hwRepoId"] "repos" '["repoId"]
          ] :=>
-        '[ "hwTitle"  ::: 'Def   :=> 'NotNull 'PGtext
-         , "hwUrl"    ::: 'NoDef :=> 'NotNull 'PGtext
+        '[ "hwId"     ::: 'Def   :=> 'NotNull 'PGint4
          , "hwRepoId" ::: 'NoDef :=> 'NotNull 'PGint4
+         , "hwTitle"  ::: 'NoDef :=> 'Null 'PGtext
+         , "hwUrl"    ::: 'NoDef :=> 'Null 'PGtext
         ])
     ]
+
+setup :: Definition '[] Schema
+setup =
+    createTable #repos
+        ( serial `as` #repoId
+        :* (text & notNullable) `as` #repoName
+        :* (text & notNullable) `as` #repoUrl
+        :* (text & notNullable) `as` #repoOwner
+        :* (SP.bool & notNullable) `as` #repoIssueHW
+        :* (SP.bool & notNullable) `as` #repoIssueGFI
+        )
+        ( primaryKey #repoId `as` #pk_repo ) >>>
+    createTable #repoIssueGFI
+        ( serial `as` #gfiId
+        :* (int & notNullable) `as` #gfiRepoId
+        :* (text & nullable) `as` #gfiTitle
+        :* (text & nullable) `as` #gfiUrl
+        )
+        ( primaryKey #gfiId `as` #pk_gfi :*
+          foreignKey #gfiRepoId #repos #repoId
+            OnDeleteCascade OnUpdateCascade `as` #fk_repo_id ) >>>
+    createTable #repoIssueHW
+        ( serial `as` #hwId
+        :* (int & notNullable) `as` #hwRepoId
+        :* (text & nullable) `as` #hwTitle
+        :* (text & nullable) `as` #hwUrl
+        )
+        ( primaryKey #hwId `as` #pk_hw :*
+          foreignKey #hwRepoId #repos #repoId
+            OnDeleteCascade OnUpdateCascade `as` #fk_repo_id )
+
+-- > printSQL setup
+-- CREATE TABLE "repos"
+--     ( "repoId" serial
+--     , "repoName" text NOT NULL
+--     , "repoUrl" text NOT NULL
+--     , "repoOwner" text NOT NULL
+--     , "repoIssueHW" bool NOT NULL
+--     , "repoIssueGFI" bool NOT NULL
+--     , CONSTRAINT "pk_repo" PRIMARY KEY ("repoId")
+--     );
+-- CREATE TABLE "repoIssueGFI"
+--     ( "gfiId" serial
+--     , "gfiRepoId" int NOT NULL
+--     , "gfiTitle" text NULL
+--     , "gfiUrl" text NULL
+--     , CONSTRAINT "pk_gfi" PRIMARY KEY ("gfiId")
+--     , CONSTRAINT "fk_repo_id" FOREIGN KEY ("gfiRepoId") REFERENCES "repos" ("repoId") ON DELETE CASCADE ON UPDATE CASCADE);
+-- CREATE TABLE "repoIssueHW"
+--     ("hwId" serial
+--     , "hwRepoId" int NOT NULL
+--     , "hwTitle" text NULL
+--     , "hwUrl" text NULL
+--     , CONSTRAINT "pk_hw" PRIMARY KEY ("hwId")
+--     , CONSTRAINT "fk_repo_id" FOREIGN KEY ("hwRepoId") REFERENCES "repos" ("repoId") ON DELETE CASCADE ON UPDATE CASCADE);
+
