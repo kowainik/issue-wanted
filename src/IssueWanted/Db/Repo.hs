9565d525549c488a70ac2bab2c871344e98c3f0f
Vladislav Sabanov <vladislav.sabanov@gmail.com>
2018-09-23 12:40:16 +0500

    Fix Schema

--- src/IssueWanted/Db/Repo.hs
+++ src/IssueWanted/Db/Repo.hs
@@ -4,31 +4,34 @@
 
 module IssueWanted.Db.Repo where
 
-import Squeal.PostgreSQL ((:::), NullityType (..), PGType (..))
-import Squeal.PostgreSQL.Schema (ColumnType (..))
-
-import qualified GHC.Generics as GHC
-
+import Squeal.PostgreSQL ((:::), NullityType (..), PGType (..), (:=>))
+import Squeal.PostgreSQL.Schema ( ColumnConstraint(..), SchemumType(..), TableConstraint(..))
 
 type Schema =
-    '[ "repos"  ::: -- all Haskell repos
-        '[ "repoId"       ::: 'Required ('NotNull 'PGint4)
-         , "repoName"     ::: 'Required ('NotNull 'PGtext)
-         , "repoUrl"      ::: 'Required ('NotNull 'PGtext)
-         , "repoOwner"    ::: 'Required ('NotNull 'PGtext)
-         , "repoIssueHW"  ::: 'Required ('NotNull 'PGbool) -- Repo has issues labeled "HelpWanted"
-         , "repoIssueGFI" ::: 'Required ('NotNull 'PGbool) -- Repo has issues labeled "Good First Issue"
-        ]
-    , "repoIssueGFI" ::: -- all Haskell issues labeled "Good First Issue"
-        '[ "gfiTitle"  ::: 'Optional ('NotNull ' PGtext)
-         , "gfiUrl"    ::: 'Optional ('NotNull 'PGtext)
-         , "gfiRepoId" ::: 'Required ('NotNull 'PGint4)
-        ]
+    '[ "repos"  ::: 'Table ( -- all Haskell repos
+        '[ "pk_repo" ::: 'PrimaryKey '["repoId"] ] :=>
+        '[ "repoId"       ::: 'NoDef :=> 'NotNull 'PGint4
+         , "repoUrl"      ::: 'NoDef :=> 'NotNull 'PGtext
+         , "repoName"     ::: 'Def   :=> 'NotNull 'PGtext
+         , "repoOwner"    ::: 'Def   :=> 'NotNull 'PGtext
+         , "repoIssueHW"  ::: 'Def   :=> 'NotNull 'PGbool -- Repo has issues labeled "HelpWanted"
+         , "repoIssueGFI" ::: 'Def   :=> 'NotNull 'PGbool -- Repo has issues labeled "Good First Issue"
+        ])
+    , "repoIssueGFI" ::: 'Table ( -- all Haskell issues labeled "Good First Issue"
+        '[ "pk_gfi"     ::: 'PrimaryKey '["gfiUrl"]
+         , "fk_repo_id" ::: 'ForeignKey '["gfiRepoId"] "repos" '["repoId"]
+         ] :=>
+        '[ "gfiTitle"  ::: 'Def   :=> 'NotNull ' PGtext
+         , "gfiUrl"    ::: 'NoDef :=> 'NotNull 'PGtext
+         , "gfiRepoId" ::: 'NoDef :=> 'NotNull 'PGint4
+        ])
 
-    , "repoIssueHW" ::: -- all Haskell issues labeled "Help Wanted"
-        '[ "hwTitle"  ::: 'Optional ('NotNull 'PGtext)
-         , "hwUrl"    ::: 'Optional ('NotNull 'PGtext)
-         , "hwRepoId" ::: 'Required ('NotNull 'PGint4)
-        ]
+    , "repoIssueHW" ::: 'Table ( -- all Haskell issues labeled "Help Wanted"
+        '[ "pk_hw"      ::: 'PrimaryKey '["hwUrl"]
+         , "fk_repo_id" ::: 'ForeignKey '["hwRepoId"] "repos" '["repoId"]
+         ] :=>
+        '[ "hwTitle"  ::: 'Def   :=> 'NotNull 'PGtext
+         , "hwUrl"    ::: 'NoDef :=> 'NotNull 'PGtext
+         , "hwRepoId" ::: 'NoDef :=> 'NotNull 'PGint4
+        ])
     ]
-
