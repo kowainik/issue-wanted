812155b8f1e3b9f3c06bae84dcbf069ebe2568e6
rashadg1030 <rashad.sasaki@gmail.com>
2019-03-14 23:17:41 -0700

    [#20] Fix bug in fetchGoodFirstIssue function
    This ensures the correct results are returned by the
    fetchGoodFirstIssue function. Before, the function would only return 8
    results. Now the function returns 832 results. This query has also
    been tested using Postman and curl.

--- src/IssueWanted/Search.hs
+++ src/IssueWanted/Search.hs
@@ -24,4 +24,4 @@ fetchHelpWanted = searchIssues "language:haskell label:\"help wanted\""
 
 -- | Fetch all issues with Haskell language and label "good-first-issue"
 fetchGoodFirstIssue :: IO (Either Error (SearchResult Issue))
-fetchGoodFirstIssue = searchIssues "language:haskell label:good-first-issue"
+fetchGoodFirstIssue = searchIssues "language:haskell label:\"good first issue\""
