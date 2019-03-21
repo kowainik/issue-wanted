452ee1557e5ad6399b9de713e644a4bc78f3fd4e
Rashad Gover <rashad.sasaki@gmail.com>
2019-03-20 23:04:13 -0700

    [#20] Fix bugs in GitHub API query functions (#22)
    * [#20] Fix bug in fetchHelpWanted function
    
    This ensures the correct results are returned by the fetchHelpWanted
    function. Before, the function would only return 8 results. Now the
    function returns 2405 results. I have tested the new query string
    using Postman and curl, and it seems to return the correct results.

--- src/IssueWanted/Search.hs
+++ src/IssueWanted/Search.hs
@@ -20,8 +20,8 @@ fetchHaskellReposGFI = searchRepos "language:haskell good-first-issues:>0"
 
 -- | Fetch all issues with Haskell language and label "help-wanted"
 fetchHelpWanted :: IO (Either Error (SearchResult Issue))
-fetchHelpWanted = searchIssues "language:haskell label:help-wanted"
+fetchHelpWanted = searchIssues "language:haskell label:\"help wanted\""
 
 -- | Fetch all issues with Haskell language and label "good-first-issue"
 fetchGoodFirstIssue :: IO (Either Error (SearchResult Issue))
-fetchGoodFirstIssue = searchIssues "language:haskell label:good-first-issue"
+fetchGoodFirstIssue = searchIssues "language:haskell label:\"good first issue\""
