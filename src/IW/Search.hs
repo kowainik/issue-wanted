a5ec4bdf88ce619af7aa39cdf7717fe1b2fa885f
rashadg1030 <rashad.sasaki@gmail.com>
2019-05-31 22:27:51 -0700

    [#5] Add new function fetchIssuesWithLabels
    Added a new function fetchIssuesWithLabels that takes a list of labels
    and returns a list of open, Haskell issues with those labels. I've also
    modified the functions fetchHelpWanted and fetchGoodFirstIssue to use
    this new function.
    
    Resolves #5

--- src/IW/Search.hs
+++ src/IW/Search.hs
@@ -5,7 +5,6 @@ module IW.Search where
 import GitHub (Error, Repo, SearchResult, Issue)
 import GitHub.Endpoints.Search (searchRepos, searchIssues)
 
-
 -- | Fetch all repositories with Haskell language
 fetchAllHaskellRepos :: IO (Either Error (SearchResult Repo))
 fetchAllHaskellRepos = searchRepos "language:haskell"
@@ -18,10 +17,17 @@ fetchHaskellReposHW = searchRepos "language:haskell help-wanted-issues:>0"
 fetchHaskellReposGFI :: IO (Either Error (SearchResult Repo))
 fetchHaskellReposGFI = searchRepos "language:haskell good-first-issues:>0"
 
+-- | Fetch all issues with Haskell language and the labels passed in to the function
+fetchIssuesWithLabels :: [Text] -> IO (Either Error (SearchResult Issue))
+fetchIssuesWithLabels labels = searchIssues $ "is:open language:haskell " <> labelQualifiers
+  where
+    labelQualifiers :: Text
+    labelQualifiers = unwords $ (\x -> " label:\"" <> x <> "\"") <$> labels   
+
 -- | Fetch all issues with Haskell language and label "help-wanted"
 fetchHelpWanted :: IO (Either Error (SearchResult Issue))
-fetchHelpWanted = searchIssues "language:haskell label:\"help wanted\""
+fetchHelpWanted = fetchIssuesWithLabels ["help wanted"]
 
 -- | Fetch all issues with Haskell language and label "good-first-issue"
 fetchGoodFirstIssue :: IO (Either Error (SearchResult Issue))
-fetchGoodFirstIssue = searchIssues "language:haskell label:\"good first issue\""
+fetchGoodFirstIssue = fetchIssuesWithLabels ["good first issue"]
