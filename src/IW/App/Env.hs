e03578924566dfb3e6762c7eb885ce1484b43f17
Rashad Gover <rashad.sasaki@gmail.com>
2019-06-13 07:26:40 -0700

    [#29] Change file structure (#38)

---
+++ src/IW/App/Env.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE AllowAmbiguousTypes  #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module IW.App.Env
+       ( Env (..)
+       , Has (..)
+       , grab
+       , DbPool
+       ) where
+    
+import Colog (HasLog (..), Message, LogAction)
+import Data.Pool (Pool)
+import Database.PostgreSQL.Simple (Connection)
+
+
+type DbPool = Pool Connection
+
+data Env (m :: Type -> Type) = Env 
+    { envDbPool    :: !DbPool
+    , envLogAction :: !(LogAction m Message)
+    } 
+
+instance HasLog (Env m) Message m where
+    getLogAction :: Env m -> LogAction m Message
+    getLogAction = envLogAction
+
+    setLogAction :: LogAction m Message -> Env m -> Env m
+    setLogAction newAction env = env { envLogAction = newAction }
+
+class Has field env where
+    obtain :: env -> field
+
+instance Has DbPool                (Env m) where obtain = envDbPool
+instance Has (LogAction m Message) (Env m) where obtain = envLogAction
+
+grab :: forall field env m . (MonadReader env m, Has field env) => m field
+grab = asks $ obtain @field
--- src/Prelude.hs
+++ src/Prelude.hs
@@ -1,7 +1,42 @@
--- | Uses [relude](https://github.com/kowainik/relude) as default Prelude.
+{-# LANGUAGE PatternSynonyms #-}
+
+-- | Uses [relude](https://hackage.haskell.org/package/relude) as default Prelude.
 
 module Prelude
        ( module Relude
+       , module Colog
+       , module Control.Lens
+       , module Json
+       , module Sql
+       , module Web
+
+       , WithLog
        ) where
 
+-- Reexport
 import Relude
+
+import Control.Lens ((.~), (^.))
+
+import Colog (pattern D, pattern E, pattern I, LogAction (..), Severity (..), pattern W, log)
+
+import Data.Aeson as Json (FromJSON (parseJSON), ToJSON (toJSON))
+
+import Database.PostgreSQL.Simple.FromField as Sql (FromField (fromField))
+import Database.PostgreSQL.Simple.FromRow as Sql (FromRow (fromRow), field)
+import Database.PostgreSQL.Simple.SqlQQ as Sql (sql)
+import Database.PostgreSQL.Simple.ToField as Sql (ToField (toField))
+import Database.PostgreSQL.Simple.ToRow as Sql (ToRow (toRow))
+import Database.PostgreSQL.Simple.Types as Sql (Only (..))
+
+import Servant.API as Web ((:>), Capture, Get, Header, Header', JSON, NoContent (NoContent), Post,
+                           QueryParam, QueryParam', ReqBody)
+import Servant.API.Generic as Web ((:-), toServant)
+import Web.HttpApiData as Web (FromHttpApiData (..), ToHttpApiData (..))
+
+-- Internal
+import qualified Colog (Message, WithLog)
+
+
+-- | 'Colog.WithLog' alias specialized to 'Message' data type.
+type WithLog env m = Colog.WithLog env Colog.Message m
