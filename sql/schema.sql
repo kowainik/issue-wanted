d64c2b81725adce6235eea35ef2a6739a8a00ca6
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-13 18:34:54 -0700

    [#30] Add foreign key constraints to join tables
    Resolves #30

--- sql/schema.sql
+++ sql/schema.sql
@@ -47,18 +47,15 @@ CREATE TABLE IF NOT EXISTS issues_labels
 , label_id INT NOT NULL  
 );
 
------------------
--- CONSTRAINTS --
------------------
+-----------------------------
+-- PRIMARY KEY CONSTRAINTS --
+-----------------------------
 
 ALTER TABLE ONLY repos 
   ADD CONSTRAINT pk_repos PRIMARY KEY (id);
 
 ALTER TABLE ONLY issues
   ADD CONSTRAINT pk_issues PRIMARY KEY (id);
-
-ALTER TABLE ONLY issues
-  ADD CONSTRAINT fk_issues FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE;
 
 ALTER TABLE ONLY categories 
   ADD CONSTRAINT pk_categories PRIMARY KEY (id);
@@ -71,3 +68,22 @@ ALTER TABLE ONLY repos_categories
 
 ALTER TABLE ONLY issues_labels
   ADD CONSTRAINT pk_issues_labels PRIMARY KEY (issue_id, label_id);
+
+-----------------------------
+-- FOREIGN KEY CONSTRAINTS --
+-----------------------------
+
+ALTER TABLE ONLY issues
+  ADD CONSTRAINT fk_repos FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY repos_categories
+  ADD CONSTRAINT fk_repos FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY repos_categories
+  ADD CONSTRAINT fk_categories FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY issues_labels
+  ADD CONSTRAINT fk_issues FOREIGN KEY (issue_id) REFERENCES issues (id) ON DELETE CASCADE;
+
+ALTER TABLE ONLY issues_labels
+  ADD CONSTRAINT fk_labels FOREIGN KEY (label_id) REFERENCES labels (id) ON DELETE CASCADE;
