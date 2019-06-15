e8ec46dd85176d15ce73f18f21efed1877c5fe6c
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-15 14:59:00 -0700

    [#26] Introduce type aliases to use for servant-generic
    Resolves #26

--- src/IW/Server/Types.hs
+++ src/IW/Server/Types.hs
@@ -1,1 +1,15 @@
-module IW.Server.Types where
+-- | This module introduce aliases to use for @servant-generic@ types and functions writing.
+
+module IW.Server.Types
+       ( AppServer
+       , ToApi
+       ) where
+
+import Servant.API.Generic (ToServantApi)
+import Servant.Server.Generic (AsServerT)
+
+import IW.App (App)
+
+
+type AppServer = AsServerT App
+type ToApi (site :: * -> *) = ToServantApi site
