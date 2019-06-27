1300b5c2a09942f3691c3ebb13d9127247a965ea
Rashad Gover <rashad.sasaki@gmail.com>
2019-06-26 23:43:12 -0700

    [#54] Update SQL schema (#56)
    Resolves #54

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,60 +6,31 @@
 -----------------
 
 CREATE TABLE IF NOT EXISTS repos
-( id    SERIAL PRIMARY KEY        
-, name  TEXT   NOT NULL
-, owner TEXT   NOT NULL
-, descr TEXT
+( id         SERIAL PRIMARY KEY  
+, owner      TEXT   NOT NULL       
+, name       TEXT   NOT NULL
+, descr      TEXT
+, categories TEXT   ARRAY
 );
 
 CREATE TABLE IF NOT EXISTS issues
-( id      SERIAL PRIMARY KEY 
-, number  INT    NOT NULL
-, title   TEXT   NOT NULL
-, body    TEXT
-, url     TEXT   NOT NULL
-, repo_id INT    NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS categories
-( id   SERIAL PRIMARY KEY
-, name TEXT   NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS labels
-( id   SERIAL PRIMARY KEY
-, name TEXT   NOT NULL
-);
-
------------------
--- JOIN TABLES --
------------------
-
-CREATE TABLE IF NOT EXISTS repos_categories
-( repo_id     INT NOT NULL
-, category_id INT NOT NULL  
-);
-
-CREATE TABLE IF NOT EXISTS issues_labels
-( issue_id INT NOT NULL
-, label_id INT NOT NULL  
+( id        SERIAL PRIMARY KEY 
+, number    INT    NOT NULL
+, title     TEXT   NOT NULL
+, body      TEXT
+, url       TEXT   NOT NULL
+, owner     TEXT   NOT NULL
+, repo_name TEXT   NOT NULL
+, labels    TEXT   ARRAY
 );
 
 -----------------------------
 -- FOREIGN KEY CONSTRAINTS --
 -----------------------------
 
+ALTER TABLE ONLY repos
+  ADD UNIQUE (owner, name);
+
 ALTER TABLE ONLY issues
-  ADD CONSTRAINT fk_repos FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY repos_categories
-  ADD CONSTRAINT fk_repos FOREIGN KEY (repo_id) REFERENCES repos (id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY repos_categories
-  ADD CONSTRAINT fk_categories FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY issues_labels
-  ADD CONSTRAINT fk_issues FOREIGN KEY (issue_id) REFERENCES issues (id) ON DELETE CASCADE;
-
-ALTER TABLE ONLY issues_labels
-  ADD CONSTRAINT fk_labels FOREIGN KEY (label_id) REFERENCES labels (id) ON DELETE CASCADE;
+  ADD CONSTRAINT fk_repos FOREIGN KEY (owner, repo_name) 
+  REFERENCES repos (owner, name) ON DELETE CASCADE;
