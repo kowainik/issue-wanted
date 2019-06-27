8d240bce216b54b88fb0e6d365dee5e478c39a61
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-27 02:10:03 -0700

    [#57] Update Issue type with FromRow instance using PGArray
    Resolves #57
    
    [#57] Add ToField instance for PGArray' newtype
    
    Resolves #57
    
    Fix after review
    
    Fix after review
    
    [#57] Move PGArray' type to its own module
    
    Resolves #57
    
    [#59] Update SQL query functions for Issue type
    
    Resolves #59
    
    [#59] Rebase
    
    Resolves #59

--- src/IW/Db/Issue.hs
+++ src/IW/Db/Issue.hs
@@ -16,22 +16,20 @@ import IW.Db.Functions (WithDb, asSingleRow, query, queryRaw)
 
 getIssues :: (WithDb env m) => m [Issue]
 getIssues = queryRaw [sql|
-    SELECT id, number, title, body, url, repo_id
+    SELECT id, number, title, body, url, owner, repo_name, labels
     FROM issues
 |]
 
 getIssueById :: (WithDb env m, WithError m) => Id Issue -> m Issue
 getIssueById issueId = asSingleRow $ query [sql|
-    SELECT id, number, title, body, url, repo_id
+    SELECT id, number, title, body, url, owner, repo_name, labels
     FROM issues
     WHERE id = ?
 |] (Only issueId)
 
 getIssuesByLabel :: (WithDb env m) => Text -> m [Issue]
 getIssuesByLabel label = query [sql|
-    SELECT issues.id, issues.number, issues.title, issues.body, issues.url, issues.repo_id
-    FROM issues_labels
-    JOIN issues ON issues.id = issues_labels.issue_id
-    JOIN labels ON labels.id = issues_labels.label_id 
-    WHERE labels.name = ?
+    SELECT id, number, title, body, url, owner, repo_name, labels 
+    FROM issues 
+    WHERE ? = ANY (labels)
 |] (Only label)
