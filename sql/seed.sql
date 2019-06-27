f52fdb7f9471e864ba956e1c68384e48ff2af00b
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-26 23:22:27 -0700

    [#54] Update SQL schema
    Resolves #54

--- sql/seed.sql
+++ sql/seed.sql
@@ -5,51 +5,43 @@
 -- BASE TABLES --
 -----------------
 
-INSERT INTO repos (name, owner, descr)
-VALUES ( 'pandoc'
-       , 'jgm'
+INSERT INTO repos (owner, name, descr, categories)
+VALUES ( 'jgm'
+       , 'pandoc'
        , 'Universal markup converter'
+       , ARRAY['Text']
        )
        ,
-       ( 'test1'
-       , 'john117'
+       ( 'john117'
+       , 'test1'
        , 'A test repo'
+       , ARRAY['Text', 'FFI']
        );
 
-INSERT INTO issues (number, title, body, url, repo_id)
+INSERT INTO issues (number, title, body, url, owner, repo_name, labels)
 VALUES ( 342
        , 'Fix docs'
        , 'Docs need to be fixed.'
        , 'https://api.github.com/repos/jgm/pandoc/issues/342'
-       , 1
+       , 'jgm'
+       , 'pandoc'
+       , ARRAY['good first issue']
        )
        ,
        ( 20
        , 'Change increment function'
        , 'Increment function should increase by two instead of one.'
        , 'https://api.github.com/repos/john117/test1/issues/20'
-       , 2
+       , 'john117'
+       , 'test1'
+       , ARRAY[] :: TEXT[]
        )
        ,
        ( 3
        , 'Update config file'
        , 'Update the server configuration file.'
        , 'https://api.github.com/repos/john117/test1/issues/3'
-       , 2
+       , 'john117'
+       , 'test1'
+       , ARRAY['good first issue', 'help wanted']
        );
-
-INSERT INTO categories (name)
-VALUES ('Text'), ('FFI');
-
-INSERT INTO labels (name)
-VALUES ('good first issue'), ('help wanted');
-
------------------
--- JOIN TABLES --
------------------
-
-INSERT INTO repos_categories (repo_id, category_id)
-VALUES (1, 1);
-
-INSERT INTO issues_labels (issue_id, label_id)
-VALUES (1, 2), (3,1), (2,2);
