306e5beb10caa4594f313512c9c55283110cbd6b
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-28 11:14:02 -0700

    [#58] Implement Repo type
    Resolves #58
    
    [#58] Add Name and Owner newtypes to Repo
    
    Resolves #58
    
    Fix after review

--- src/IW/Core/Repo.hs
+++ src/IW/Core/Repo.hs
@@ -1,7 +1,21 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
 module IW.Core.Repo
-       ( Repo
+       ( Repo (..)
        ) where
+
+import IW.Core.Id (Id (..))
+import IW.Core.RepoName (RepoName (..))
+import IW.Core.RepoOwner (RepoOwner (..))
+import IW.Core.SqlArray (SqlArray (..))
         
 
--- | Data type representing a GitHub repository.
-data Repo
+-- | Data type representing a GitHub repository
+data Repo = Repo 
+    { repoId         :: Id Repo
+    , repoOwner      :: RepoOwner
+    , repoName       :: RepoName
+    , repoDescr      :: Text
+    , repoCategories :: SqlArray Text
+    } deriving stock (Generic, Show, Eq)
+      deriving anyclass (ToJSON, FromRow, ToRow)
