459a81a0410bab3fe4087080eed21521678ae11f
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-28 09:45:58 -0700

    [#59] Add documentation
    Resolves #59

--- src/IW/Db/Issue.hs
+++ src/IW/Db/Issue.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 
--- | SQL queries to work with the @users@ table.
+-- | SQL queries to work with the @issues@ table.
 
 module IW.Db.Issue
        ( getIssues
@@ -13,13 +13,14 @@ import IW.Core.Issue (Issue (..))
 import IW.Core.Id (Id (..))
 import IW.Db.Functions (WithDb, asSingleRow, query, queryRaw)
 
-
+-- | Returns all issues in the database
 getIssues :: (WithDb env m) => m [Issue]
 getIssues = queryRaw [sql|
     SELECT id, number, title, body, url, owner, repo_name, labels
     FROM issues
 |]
 
+-- | Returns a single issue with the corresponding ID
 getIssueById :: (WithDb env m, WithError m) => Id Issue -> m Issue
 getIssueById issueId = asSingleRow $ query [sql|
     SELECT id, number, title, body, url, owner, repo_name, labels
@@ -27,6 +28,7 @@ getIssueById issueId = asSingleRow $ query [sql|
     WHERE id = ?
 |] (Only issueId)
 
+-- | Returns a list of issues filtered by label name
 getIssuesByLabel :: (WithDb env m) => Text -> m [Issue]
 getIssuesByLabel label = query [sql|
     SELECT id, number, title, body, url, owner, repo_name, labels 
