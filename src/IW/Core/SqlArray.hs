6bb4d6fba1f998d3680730ccc21ad9944a876033
rashadg1030 <rashad.sasaki@gmail.com>
2019-06-27 22:21:39 -0700

    [#57] Replace custom instances of SqlArray with deriving via
    Resolves #57

--- src/IW/Core/SqlArray.hs
+++ src/IW/Core/SqlArray.hs
@@ -5,18 +5,9 @@ module IW.Core.SqlArray
     ) where
 
 import Database.PostgreSQL.Simple.Types (PGArray (..))
-import Database.PostgreSQL.Simple.FromField (FieldParser, pgArrayFieldParser)
-import Database.PostgreSQL.Simple.ToField (Action (..))
 
 
 newtype SqlArray a = SqlArray { unSqlArray :: [a] }
     deriving stock   (Generic, Show)
     deriving newtype (Eq, ToJSON)
-
-instance (FromField a, Typeable a) => FromField (SqlArray a) where
-    fromField :: FieldParser (SqlArray a)
-    fromField f dat = SqlArray . fromPGArray <$> pgArrayFieldParser fromField f dat
-
-instance (ToField a) => ToField (SqlArray a) where
-    toField :: SqlArray a -> Action
-    toField = toField . PGArray . unSqlArray 
+    deriving (ToField, FromField) via PGArray a
