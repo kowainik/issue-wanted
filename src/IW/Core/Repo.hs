14d17c5d2ba871bfad1b53ebf3ee309288a060dd
Rashad Gover <rashad.sasaki@gmail.com>
2019-06-29 01:52:22 -0700

    [#58] Implement Repo type (#69)

--- src/IW/Core/Repo.hs
+++ src/IW/Core/Repo.hs
@@ -1,7 +1,29 @@
-module IW.Core.Repo
-       ( Repo
-       ) where
-        
+{-# LANGUAGE DeriveAnyClass #-}
 
--- | Data type representing a GitHub repository.
-data Repo
+module IW.Core.Repo
+       ( Repo (..)
+       ) where
+
+import IW.Core.Id (Id (..))
+import IW.Core.SqlArray (SqlArray (..))
+   
+
+-- | Wrapper for repository name
+newtype RepoName = RepoName { unRepoName :: Text }
+    deriving stock (Show, Generic)
+    deriving newtype (Eq, Ord, FromField, ToField, FromJSON, ToJSON, FromHttpApiData)
+
+-- | Wrapper for repository owner
+newtype RepoOwner = RepoOwner { unRepoOwner :: Text }
+    deriving stock (Show, Generic)
+    deriving newtype (Eq, Ord, FromField, ToField, FromJSON, ToJSON, FromHttpApiData)
+
+-- | Data type representing a GitHub repository
+data Repo = Repo 
+    { repoId         :: Id Repo
+    , repoOwner      :: RepoOwner
+    , repoName       :: RepoName
+    , repoDescr      :: Text
+    , repoCategories :: SqlArray Text
+    } deriving stock (Generic, Show, Eq)
+      deriving anyclass (ToJSON, FromRow, ToRow)
