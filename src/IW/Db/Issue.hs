f122960e505aaaa0ff6658ff5ee4932e697b2101
Rashad Gover <rashad.sasaki@gmail.com>
2019-06-29 00:24:21 -0700

    [#59] Update SQL query functions for Issue type (#67)
    * [#57] Update Issue type with FromRow instance using PGArray
    
    Resolves #57
    
    [#57] Add ToField instance for PGArray' newtype
    
    Resolves #57
    
    Fix after review
    
    Fix after review
    
    [#57] Move PGArray' type to its own module
    
    Resolves #57
    
    [#59] Update SQL query functions for Issue type
    
    Resolves #59
    
    [#59] Rebase
    
    Resolves #59
    
    * [#59] Add documentation
    
    Resolves #59

--- src/IW/Db/Issue.hs
+++ src/IW/Db/Issue.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE QuasiQuotes #-}
 
--- | SQL queries to work with the @users@ table.
+-- | SQL queries to work with the @issues@ table.
 
 module IW.Db.Issue
        ( getIssues
@@ -13,25 +13,25 @@ import IW.Core.Issue (Issue (..))
 import IW.Core.Id (Id (..))
 import IW.Db.Functions (WithDb, asSingleRow, query, queryRaw)
 
-
+-- | Returns all issues in the database
 getIssues :: (WithDb env m) => m [Issue]
 getIssues = queryRaw [sql|
-    SELECT id, number, title, body, url, repo_id
+    SELECT id, number, title, body, url, owner, repo_name, labels
     FROM issues
 |]
 
+-- | Returns a single issue with the corresponding ID
 getIssueById :: (WithDb env m, WithError m) => Id Issue -> m Issue
 getIssueById issueId = asSingleRow $ query [sql|
-    SELECT id, number, title, body, url, repo_id
+    SELECT id, number, title, body, url, owner, repo_name, labels
     FROM issues
     WHERE id = ?
 |] (Only issueId)
 
+-- | Returns a list of issues filtered by label name
 getIssuesByLabel :: (WithDb env m) => Text -> m [Issue]
 getIssuesByLabel label = query [sql|
-    SELECT issues.id, issues.number, issues.title, issues.body, issues.url, issues.repo_id
-    FROM issues_labels
-    JOIN issues ON issues.id = issues_labels.issue_id
-    JOIN labels ON labels.id = issues_labels.label_id 
-    WHERE labels.name = ?
+    SELECT id, number, title, body, url, owner, repo_name, labels 
+    FROM issues 
+    WHERE ? = ANY (labels)
 |] (Only label)
