4b360c46db458df0975d495acd1b617788b98e22
Rashad Gover <rashad.sasaki@gmail.com>
2019-07-01 20:29:31 -0700

    [#73] Add created_at and updated_at columns (#80)

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,21 +6,25 @@
 -----------------
 
 CREATE TABLE IF NOT EXISTS repos
-( id         SERIAL PRIMARY KEY  
-, owner      TEXT   NOT NULL       
-, name       TEXT   NOT NULL
-, descr      TEXT
-, categories TEXT   ARRAY
+( id         SERIAL    PRIMARY KEY  
+, owner      TEXT      NOT NULL       
+, name       TEXT      NOT NULL
+, descr      TEXT      NOT NULL
+, categories TEXT      ARRAY
+, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+, updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS issues
-( id         SERIAL PRIMARY KEY 
-, number     INT    NOT NULL
-, title      TEXT   NOT NULL
-, body       TEXT
-, repo_owner TEXT   NOT NULL
-, repo_name  TEXT   NOT NULL
-, labels     TEXT   ARRAY
+( id         SERIAL    PRIMARY KEY 
+, number     INT       NOT NULL
+, title      TEXT      NOT NULL
+, body       TEXT      NOT NULL
+, repo_owner TEXT      NOT NULL
+, repo_name  TEXT      NOT NULL
+, labels     TEXT      ARRAY
+, created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
+, updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 
 -----------------------------
@@ -33,3 +37,18 @@ ALTER TABLE ONLY repos
 ALTER TABLE ONLY issues
   ADD CONSTRAINT fk_repos FOREIGN KEY (repo_owner, repo_name) 
   REFERENCES repos (owner, name) ON DELETE CASCADE;
+
+--------------
+-- TRIGGERS --
+--------------
+
+CREATE OR REPLACE FUNCTION update_updated_at() 
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW; 
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER update_updated_at_repos BEFORE UPDATE ON repos FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
+CREATE TRIGGER update_updated_at_issues BEFORE UPDATE ON issues FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
