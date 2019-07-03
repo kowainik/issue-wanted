4c108bee1be468095295bd01f157037e4703a583
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-03 15:12:03 -0700

    [#61] Add conditional insert for issues
    Resolves #61

--- sql/seed.sql
+++ sql/seed.sql
@@ -18,29 +18,38 @@ VALUES ( 'jgm'
        , ARRAY['Text', 'FFI']
        );
 
+insert into issues (repo_owner, repo_name, number, title, body, labels)
+select 'jgm', 'pandoc', 342, 'Fix docs', 'Docs need to be fixed.', ARRAY['good first issue']
+where exists (
+    select * from repos where repos.owner = NEW.repo_owner and repos.name = NEW.repo_name
+)
+ON CONFLICT (repo_owner, repo_name, number) DO 
+UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
+
+insert into issues (repo_owner, repo_name, number, title, body, labels)
+select 'marry5454', 'test0', 6, 'Update config file', 'Update the server configuration file.', ARRAY['good first issue']
+where exists (
+    select * from repos where repos.owner = 'marry5454' and repos.name = 'test0'
+) 
+ON CONFLICT (repo_owner, repo_name, number) DO 
+UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
+
 INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
-VALUES ( 'jgm'
-       , 'pandoc'
-       , 342
-       , 'Fix docs'
-       , 'Docs need to be fixed.'
-       , ARRAY['good first issue']
-       )
-       ,
-       ( 'john117'
-       , 'test1'
-       , 20
-       , 'Change increment function'
-       , 'Increment function should increase by two instead of one.'
-       , ARRAY[] :: TEXT[]
-       )
-       ,
-       ( 'john117'
+VALUES ( 'john117'
        , 'test1'
        , 3
        , 'Update config file'
        , 'Update the server configuration file.'
        , ARRAY['good first issue', 'help wanted']
+       ) ON CONFLICT DO NOTHING;
+
+INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
+VALUES ( 'john117'
+       , 'test1'
+       , 20
+       , 'Change increment function'
+       , 'Increment function should increase by two instead of one.'
+       , ARRAY[] :: TEXT[]
        );
 
 -------------
