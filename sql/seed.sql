533f7b52b90dc5b6f902a705acc4ebb88937ab01
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-03 18:37:04 -0700

    [#61] Update UPSERT statements for issues
    Resolves #61

--- sql/seed.sql
+++ sql/seed.sql
@@ -18,60 +18,28 @@ VALUES ( 'jgm'
        , ARRAY['Text', 'FFI']
        );
 
-insert into issues (repo_owner, repo_name, number, title, body, labels)
-select 'jgm', 'pandoc', 342, 'Fix docs', 'Docs need to be fixed.', ARRAY['good first issue']
-where exists (
-    select * from repos where repos.owner = NEW.repo_owner and repos.name = NEW.repo_name
+INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
+SELECT repo_owner, repo_name, number, title, body, labels
+FROM ( VALUES ('jgm','pandoc',342,'Fix docs','Docs need to be fixed.',ARRAY['good first issue','low hanging fruit','docs'])
+     , ('marry5454', 'test0', 6, 'Update config file', 'Update the server configuration file.', ARRAY['good first issue'])
+     , ('john117', 'test1', 3, 'Update config file', 'Update the server configuration file.', ARRAY['good first issue', 'help wanted'])
+     )
+AS new (repo_owner, repo_name, number, title, body, labels)
+WHERE EXISTS (
+    SELECT * FROM repos WHERE (repos.owner, repos.name) = (new.repo_owner, new.repo_name)
 )
 ON CONFLICT (repo_owner, repo_name, number) DO 
 UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
 
-insert into issues (repo_owner, repo_name, number, title, body, labels)
-select 'marry5454', 'test0', 6, 'Update config file', 'Update the server configuration file.', ARRAY['good first issue']
-where exists (
-    select * from repos where repos.owner = 'marry5454' and repos.name = 'test0'
-) 
-ON CONFLICT (repo_owner, repo_name, number) DO 
-UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
 
 INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
-VALUES ( 'john117'
-       , 'test1'
-       , 3
-       , 'Update config file'
-       , 'Update the server configuration file.'
-       , ARRAY['good first issue', 'help wanted']
-       ) ON CONFLICT DO NOTHING;
-
-INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
-VALUES ( 'john117'
-       , 'test1'
-       , 20
-       , 'Change increment function'
-       , 'Increment function should increase by two instead of one.'
-       , ARRAY[] :: TEXT[]
-       );
-
--------------
--- UPSERTS --
--------------
-
-INSERT INTO repos (owner, name, descr, categories)
-VALUES ( 'john117'
-       , 'test1'
-       , 'An experimental repo'
-       , ARRAY['Text', 'FFI', 'NLP']
-       )
-ON CONFLICT (owner, name) DO 
-UPDATE SET descr = EXCLUDED.descr, categories = EXCLUDED.categories;
-
-INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
-VALUES ( 'jgm'
-       , 'pandoc'
-       , 342
-       , 'Fix docs'
-       , 'Docs need to be fixed. Especially the section on building the project.'
-       , ARRAY['good first issue', 'low hanging fruit', 'docs']
-       )
+SELECT repo_owner, repo_name, number, title, body, labels
+FROM ( VALUES ('john117', 'test1', 3, 'An experimental repo', 'Update the server configuration file.' , ARRAY['Text', 'FFI', 'NLP'])
+     , ('jgm', 'pandoc', 342, 'Fix docs', 'Docs need to be fixed. Especially the section on building the project.', ARRAY['good first issue'])
+     )
+AS new (repo_owner, repo_name, number, title, body, labels)
+WHERE EXISTS (
+    SELECT * FROM repos WHERE repos.owner = new.repo_owner AND repos.name = new.repo_name
+)
 ON CONFLICT (repo_owner, repo_name, number) DO 
 UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
