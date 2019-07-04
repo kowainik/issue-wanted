2f82915d597d67e5e03dbdba6eb50353ca4bd9ca
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-03 19:22:41 -0700

    [#61] Add UPSERT statements for repos table
    Resolves #61

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,22 +1,20 @@
 -- To execute this file from SQL REPL:
 -- \i sql/seed.sql
 
------------------
--- BASE TABLES --
------------------
+-------------
+-- UPSERTS --
+-------------
 
 INSERT INTO repos (owner, name, descr, categories)
-VALUES ( 'jgm'
-       , 'pandoc'
-       , 'Universal markup converter'
-       , ARRAY['Text']
-       )
-       ,
-       ( 'john117'
-       , 'test1'
-       , 'A test repo'
-       , ARRAY['Text', 'FFI']
-       );
+VALUES ('jgm', 'pandoc', 'Universal markup converter', ARRAY['Text', 'Parser'])
+     , ('john117', 'test1', 'A test repo', ARRAY['Text', 'FFI'])
+ON CONFLICT ON CONSTRAINT unique_repos DO 
+UPDATE SET descr = EXCLUDED.descr, categories = EXCLUDED.categories;
+
+INSERT INTO repos (owner, name, descr, categories)
+VALUES ('jgm', 'pandoc', 'Universal markup converter for various text formats.', ARRAY['Text'])
+ON CONFLICT ON CONSTRAINT unique_repos DO 
+UPDATE SET descr = EXCLUDED.descr, categories = EXCLUDED.categories;
 
 INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
 SELECT repo_owner, repo_name, number, title, body, labels
@@ -30,7 +28,6 @@ WHERE EXISTS (
 )
 ON CONFLICT ON CONSTRAINT unique_issues DO 
 UPDATE SET title = EXCLUDED.title, body = EXCLUDED.body, labels = EXCLUDED.labels;
-
 
 INSERT INTO issues (repo_owner, repo_name, number, title, body, labels)
 SELECT repo_owner, repo_name, number, title, body, labels
