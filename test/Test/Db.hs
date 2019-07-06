017519443089b2d0a107064ea42d7e01db2b0cc0
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-06 11:14:06 -0700

    [#89] Add tagWithId function for adding an Id to a value
    Resolves #89

--- test/Test/Db.hs
+++ test/Test/Db.hs
@@ -31,6 +31,9 @@ beforeAfter getter comparison action = do
     after <- getter
     pure $ after `comparison` before
 
+tagWithId :: Int -> a -> WithId a
+tagWithId valId val = WithId (Id valId) val
+
 ---------------
 -- REPO SPEC --
 ---------------
@@ -49,7 +52,7 @@ upsertReposSpec env = describe "upsertRepos" $ do
     it "should insert repos if the repos are valid" $
         env & reposIncreased (upsertRepos [validRepo]) `equals` True
     it "should update the repo if the same repo already exists" $
-        env & reposRowDifference (upsertRepos [updatedValidRepo]) `equals` [WithId (Id 1) updatedValidRepo]
+        env & reposRowDifference (upsertRepos [updatedValidRepo]) `equals` zipWith tagWithId [1..] [updatedValidRepo]
 
 getReposSpec :: AppEnv -> Spec
 getReposSpec env = describe "getRepos" $
@@ -97,7 +100,7 @@ upsertIssuesSpec env = describe "upsertIssues" $ do
     it "should leave the issues table unaffected when there's no corresponding repo" $
         env & issuesUnaffected (upsertIssues [invalidIssue]) `equals` True 
     it "should update the issue if the same issue already exists" $
-        env & issuesRowDifference (upsertIssues [updatedValidIssue]) `equals` [WithId (Id 1) updatedValidIssue]
+        env & issuesRowDifference (upsertIssues [updatedValidIssue]) `equals` zipWith tagWithId [1..] [updatedValidIssue]
 
 getIssuesSpec :: AppEnv -> Spec
 getIssuesSpec env = describe "getIssues" $
