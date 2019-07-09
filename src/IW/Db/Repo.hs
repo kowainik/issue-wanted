6da0051c9973dba8251f644260be7e59a5bab8d2
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-08 23:32:47 -0700

    [#97] Add LIMIT 100 to DB query functions
    Resolves #97

--- src/IW/Db/Repo.hs
+++ src/IW/Db/Repo.hs
@@ -18,6 +18,7 @@ getRepos :: (WithDb env m) => m [WithId Repo]
 getRepos = queryRaw [sql|
     SELECT id, owner, name, descr, categories
     FROM repos
+    LIMIT 100
 |]
 
 -- | Returns a list of repos filtered by category name
@@ -26,6 +27,7 @@ getReposByCategory = query [sql|
     SELECT id, owner, name, descr, categories
     FROM repos
     WHERE ? = ANY (categories)
+    LIMIT 100
 |] . Only
 
 -- | Insert a list of repos into the database, but update on conflict
