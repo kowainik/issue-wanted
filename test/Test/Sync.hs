567cbbde810e3881486b6260167c189644237a3d
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-10 09:16:46 -0700

    Fix after review

--- test/Test/Sync.hs
+++ test/Test/Sync.hs
@@ -9,33 +9,27 @@ import Test.Hspec (Spec, describe, it, shouldBe)
 import qualified GitHub  
 
 
-testGitHubIssueUrl :: GitHub.URL
-testGitHubIssueUrl = GitHub.URL "https://api.github.com/repos/owner123/repo123/issues/1"
-
-testBadGitHubIssueUrl1 :: GitHub.URL
-testBadGitHubIssueUrl1 = GitHub.URL "https://api.github.com/repos/owner123/issues/1"
-
-testBadGitHubIssueUrl2 :: GitHub.URL
-testBadGitHubIssueUrl2 = GitHub.URL "https://api.github.com/randomWord/owner123/repo123/issues/1"
-
-testBadGitHubIssueUrl3 :: GitHub.URL
-testBadGitHubIssueUrl3 = GitHub.URL "api.github.com/repos/owner123/repo123/issues/1"
-
 syncSpecs :: Spec
 syncSpecs = describe "GitHub sync correctness"
     parseIssueUserDataSpec
 
 parseIssueUserDataSpec :: Spec
 parseIssueUserDataSpec = describe "parseIssueUserData" $ do
-    it "should return Just (RepoOwner owner123, RepoName repo123)" $ 
+    it "parsing testGitHubIssueUrl return Just (RepoOwner owner123, RepoName repo123)" $ 
         parseIssueUserData testGitHubIssueUrl 
             `shouldBe` Just (RepoOwner "owner123", RepoName "repo123")
-    it "should return Nothing" $ 
+    it "parsing testBadGitHubIssueUrl1 should return Nothing" $ 
         parseIssueUserData testBadGitHubIssueUrl1
             `shouldBe` Nothing
-    it "should return Nothing" $
+    it "parsing testBadGitHubIssueUrl2 return Nothing" $
         parseIssueUserData testBadGitHubIssueUrl2
             `shouldBe` Nothing
-    it "should return Nothing" $
-        parseIssueUserData testBadGitHubIssueUrl3
-            `shouldBe` Nothing
+
+testGitHubIssueUrl :: GitHub.URL
+testGitHubIssueUrl = GitHub.URL "https://api.github.com/repos/owner123/repo123/issues/1"
+
+testBadGitHubIssueUrl1 :: GitHub.URL
+testBadGitHubIssueUrl1 = GitHub.URL "https://api.github.com/randomWord/owner123/repo123/issues/1"
+
+testBadGitHubIssueUrl2 :: GitHub.URL
+testBadGitHubIssueUrl2 = GitHub.URL "api.github.com/repos/owner123/repo123/issues/1"
