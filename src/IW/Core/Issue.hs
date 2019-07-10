ede9c4b4098d2459055dfab120f247e3865da206
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-09 20:58:42 -0700

    [#107] Add Url newtype for wrapping URL text
    Resolves #107
    
    Fix after review

--- src/IW/Core/Issue.hs
+++ src/IW/Core/Issue.hs
@@ -8,12 +8,13 @@ module IW.Core.Issue
   
 import IW.Core.SqlArray (SqlArray (..))
 import IW.Core.Repo (RepoName (..), RepoOwner (..))
+import IW.Core.Url (Url (..))
 
 
 -- | Wrapper for issue label names.
 newtype Label = Label { unLabel :: Text }
-    deriving stock   (Generic, Show)
-    deriving newtype (Eq, Ord, FromField, ToField, FromJSON, ToJSON, FromHttpApiData)
+    deriving stock   (Generic)
+    deriving newtype (Eq, Ord, Show, FromField, ToField, FromJSON, ToJSON, FromHttpApiData)
 
 -- | Data type representing a GitHub issue.
 data Issue = Issue
@@ -26,12 +27,12 @@ data Issue = Issue
     } deriving stock (Eq, Generic, Show)
       deriving anyclass (ToJSON, FromRow, ToRow)
 
-issueUrl :: Issue -> Text
-issueUrl Issue{..} = 
-    "https://github.com/" 
+issueUrl :: Issue -> Url
+issueUrl Issue{..} = Url  
+    $ "https://github.com/" 
     <> unRepoOwner issueRepoOwner 
     <> "/" 
     <> unRepoName issueRepoName 
     <> "/issues/" 
     <> show issueNumber
-    
+ 
--- src/IW/Core/SqlArray.hs
+++ src/IW/Core/SqlArray.hs
@@ -6,6 +6,6 @@ import Database.PostgreSQL.Simple.Types (PGArray (..))
 
 
 newtype SqlArray a = SqlArray { unSqlArray :: [a] }
-    deriving stock   (Generic, Show)
-    deriving newtype (Eq, ToJSON)
+    deriving stock   (Generic)
+    deriving newtype (Eq, ToJSON, Show)
     deriving (ToField, FromField) via PGArray a
