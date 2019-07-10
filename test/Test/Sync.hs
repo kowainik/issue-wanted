db7a6df7222819bf40cc6e9acbf12ce4c8a3f8d7
rashadg1030 <rashad.sasaki@gmail.com>
2019-07-09 20:58:42 -0700

    [#107] Add Url newtype for wrapping URL text
    Resolves #107
    
    [#105] Introduce MonadDownload and Manager type
    
    Resolves #105
    
    [#105] Add Manager field to Env type
    
    Resolves #105
    
    [#105] Add WithDownload type constraint
    
    Resolves #105
    
    [#105] Implement downloadFile for MonadDownload App instance
    
    Resolves #105
    
    Fix after review
    
    Fix after review
    
    Fix after review
    
    Fix after review

--- test/Test/Sync.hs
+++ test/Test/Sync.hs
@@ -2,16 +2,22 @@ module Test.Sync
        ( syncSpecs
        ) where
 
-import IW.Core.Repo (RepoOwner (..), RepoName (..))
-import IW.Sync.Search (parseIssueUserData)
 import Test.Hspec (Spec, describe, it, shouldBe)
+
+import IW.App (AppEnv, notFound)
+import IW.Core.Repo (RepoOwner (..), RepoName (..))
+import IW.Core.Url (Url (..))
+import IW.Effects.Download (downloadFileImpl)
+import IW.Sync.Search (parseIssueUserData)
+import Test.Assert (succeeds, failsWith)
 
 import qualified GitHub  
 
 
-syncSpecs :: Spec
-syncSpecs = describe "GitHub sync correctness"
+syncSpecs :: AppEnv -> Spec
+syncSpecs env = describe "GitHub sync correctness" $ do
     parseIssueUserDataSpec
+    downloadFileSpec env 
 
 parseIssueUserDataSpec :: Spec
 parseIssueUserDataSpec = describe "parseIssueUserData" $ do
@@ -33,3 +39,16 @@ testBadGitHubIssueUrl1 = GitHub.URL "https://api.github.com/randomWord/owner123/
 
 testBadGitHubIssueUrl2 :: GitHub.URL
 testBadGitHubIssueUrl2 = GitHub.URL "api.github.com/repos/owner123/repo123/issues/1"
+
+downloadFileSpec :: AppEnv -> Spec
+downloadFileSpec env = describe "downloadFile" $ do
+    it "should succeed with 200 status code when passed a valid Url" $
+       env & succeeds (downloadFileImpl issueWantedUrl)
+    it "should fail with notFound error when passed a non-existent Url" $
+       env & downloadFileImpl nonExistentUrl `failsWith` notFound 
+
+issueWantedUrl :: Url
+issueWantedUrl = Url "https://raw.githubusercontent.com/kowainik/issue-wanted/master/issue-wanted.cabal"
+
+nonExistentUrl :: Url
+nonExistentUrl = Url "https://raw.githubusercontent.com/blahblah/noexist123/master/noexist123.kabal"
